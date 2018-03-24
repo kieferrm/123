cecd77149af68b2c72a5e35e90128ac320c4903e
kieferrm <kieferrm@gmail.com>
2018-03-24 14:35:02 -0700

    path changes

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const path = require('path')
 const app = express()
 
-app.use('/static', express.static(path.join(__dirname, 'static')))
-app.get('/', (req, res) => res.send('Hello World!'))
+app.use('/', express.static(path.join(__dirname, 'static')))
+app.get('/hello', (req, res) => res.send('Hello World!'))
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
