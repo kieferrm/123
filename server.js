3a205bdde376e34dac2700c0efa7094a8feed152
kieferrm <kieferrm@gmail.com>
2018-03-24 17:45:21 -0700

    testing environment

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ const path = require('path')
 const app = express()
 
 app.use('/', express.static(path.join(__dirname, 'static')))
-app.get('/hello', (req, res) => res.send('Hello World!'))
+app.get('/hello', (req, res) => res.send(`Hello ${process.env['RWAF_CUSTOM']}!`))
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
